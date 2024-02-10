d95f855755dc6bd6a33c46d89e9b8090608a357a
Sk. Azadur Rahman <32958484+astutecoder@users.noreply.github.com>
2024-02-10 21:41:55 +0600

    Update IRepository.ts

--- src/core/IRepository.ts
+++ src/core/IRepository.ts
@@ -1,9 +1,7 @@
-import { IResponse } from './IResponse';
-
 export interface IRepository<T> {
   find(query: Record<string, unknown>): Promise<T[]>;
   findById(id: string): Promise<T | null>;
-  create(data: T): Promise<T | null>;
+  create(data: Partial<T>): Promise<T | null>;
   update(id: string, data: Partial<T>): Promise<T | null>;
-  remove(id: string): Promise<T | null>;
+  remove(id: string): Promise<null | T>;
 }
